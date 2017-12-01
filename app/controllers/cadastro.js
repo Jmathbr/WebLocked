6f3251706d8d60fbbdbc64f592069dfd9a841344
jmathbr <joao.matheus92225@gmail.com>
2017-11-30 23:32:34 -0300

    formulario atualizado

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,23 @@
 module.exports.cadastro = function(app, req, res){
     res.render('usrq/cadastro');
 }
+
+module.exports.cadastrar = function(app, req, res){
+    var dadosForm = req.body;
+
+    req.assert('nome','nome Nao pode ser vazio').notEmpty();
+    req.assert('email','Email Nao pode ser vazio').notEmpty();
+    req.assert('matricula','idade Nao pode ser vazio').notEmpty();
+    req.assert('date','data de nascimento Nao pode ser vazio').notEmpty();
+    req.assert('contato','contato Nao pode ser vazio').notEmpty();
+    req.assert('projeto','projeto Nao pode ser vazio').notEmpty();
+    
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.send('Existe erros no formulario de cadastro')
+        return;
+    }
+    res.send('possivel cadastrar')
+}
