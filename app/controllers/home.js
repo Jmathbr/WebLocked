4c5f5bc7feb14cc2b34b1fa117da16627c0ed32d
jmathbr <joao.matheus92225@gmail.com>
2017-12-04 17:48:29 -0300

    Login sendo feito, express-session

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,18 @@
 module.exports.home = function(app, req, res){
     res.render('home/index');
 }
+module.exports.autenticar = function(app, req, res){
+
+    var dadosForm = req.body;
+    
+    req.assert('email','Email nao pode ser vazio').notEmpty();
+    req.assert('senha','Senha nao pode ser vazio').notEmpty();
+    
+    var erros = req.validationErrors();
+    
+        if(erros){
+            res.render('home/index',{validacao: erros, dadosForm: dadosForm})
+            return;
+        }
+        res.send('Sessao criada')
+}
--- app/routes/login.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-    app.get('/login', function(req,res){
-        app.app.controllers.login.login(app, req, res)
-    })
-}
