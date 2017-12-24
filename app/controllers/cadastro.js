3e6c45240dac190bf345a2e38e9d1a893a9592cb
jmathbr <joao.matheus92225@gmail.com>
2017-12-23 22:56:53 -0300

    tratamento de cadastro no back-end

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -7,12 +7,9 @@ module.exports.cadastrar = function(app, req, res){
     var dadosForm = req.body;
 
     req.assert('nome','Nome nao pode ser vazio').notEmpty();
-    req.assert('email','Email nao pode ser vazio').notEmpty();
-    req.assert('matricula','Idade nao pode ser vazio').notEmpty();
-    req.assert('date','data de nascimento nao pode ser vazio').notEmpty();
-    req.assert('contato','Contato nao pode ser vazio').notEmpty();
-    req.assert('senha','Senha nao pode ser vazio').notEmpty();
-    req.assert('senha','As senhas nao sao iguais').equals(dadosForm.rsenha);
+    req.assert('email','Email nao pode ser vazio').isEmail();
+    req.assert('matricula','Idade nao pode ser vazio').isNumeric();
+    req.assert('senha','As senhas nao sao iguais, ou os diretorios estao vazios').equals(dadosForm.rsenha);
 
     var erros = req.validationErrors();
 
@@ -20,7 +17,7 @@ module.exports.cadastrar = function(app, req, res){
         res.render('usrq/cadastro',{validacao: erros, dadosForm: dadosForm})
         return;
     }
-    dadosForm.classe = '';
+    dadosForm.classe = 'C';
     dadosForm.last = '';
     var connection = app.config.dbConnection;
     var UsuariosDAO = new app.app.models.UsuariosDAO(connection);
